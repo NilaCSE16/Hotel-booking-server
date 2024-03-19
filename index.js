d715b57321d2e9af5ffa83d3f1bcc8082a818c61
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-03-19 16:32:34 +0600

    set cookies and jwt validation

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.port || 5000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
@@ -23,15 +31,71 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log("Token: ", token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access!!!!!!!" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "An Unauthorized Access" });
+    }
+    req.user = decoded;
+    // console.log("Decoded: ", decoded.email);
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("hotelDB").collection("users");
     const roomCollection = client.db("hotelDB").collection("rooms");
+    const blogCollection = client.db("hotelDB").collection("blogs");
+    const bookingCollection = client.db("hotelDB").collection("bookings");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log("User: ", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/addUsers", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      let query = {};
+      // console.log(req.query.email);
+      if (req.query.email) {
+        query = { email: req.query.email };
+      }
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/addRoom", async (req, res) => {
       const room = req.body;
-      console.log(room);
+      // console.log(room);
       const result = await roomCollection.insertOne(room);
       res.send(result);
     });
@@ -43,6 +107,80 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await roomCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/addBookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.get("/bookings", verifyToken, async (req, res) => {
+      let query = {};
+      // console.log("Nilaaaaaaa: ", req.user.email);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Invalid User" });
+      }
+      if (req.query.roomId) {
+        query = { roomId: req.query.roomId };
+      } else {
+        if (req.query.email) {
+          query = { email: req.query.email };
+        }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.put("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const booking = req.body;
+      const options = { upsert: true };
+      console.log("Booking: ", booking);
+      const updateDoc = {
+        $set: {
+          date: booking.date,
+        },
+      };
+      const result = await bookingCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.delete("/bookings", async (req, res) => {
+      // const today = new Date().toISOString().split("T")[0];
+      const today = new Date();
+      const month = String(today.getMonth() + 1).padStart(2, "0"); // Month starts from 0
+      const day = String(today.getDate()).padStart(2, "0");
+      const year = today.getFullYear();
+
+      const formattedDate = `${month}/${day}/${year}`;
+
+      // console.log(formattedDate); // Output: mm/dd/yyyy
+
+      // console.log("Today: ", today);
+      const result = await bookingCollection.deleteMany({
+        date: { $lt: formattedDate },
+      });
+      res.send(result);
+    });
+
+    app.post("/addBlogs", async (req, res) => {
+      const blog = req.body;
+      console.log(blog);
+      const result = await blogCollection.insertOne(blog);
+      res.send(result);
+    });
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollection.find().toArray();
       res.send(result);
     });
 
