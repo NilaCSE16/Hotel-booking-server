0545d3cc0a41086c5e2a3a4ce575504975ad61e7
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-04-02 09:37:57 +0600

    change origin

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://hotel-booking-client-tau.vercel.app"],
+    origin: [
+      "https://hotel-booking-client-tau.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
