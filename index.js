27e07bbf6ffb345693c7b3dcc0b56cf144c6dd2d
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-04-29 20:33:03 +0600

    change vercel file

--- index.js
+++ index.js
@@ -63,14 +63,14 @@ async function run() {
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       // console.log("User: ", user);
-      const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
+      const token = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
         expiresIn: "1h",
       });
-      const refreshToken = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
-        expiresIn: "1h",
-      });
+      // const refreshToken = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
+      //   expiresIn: "1h",
+      // });
       res
-        .cookie("refreshToken", accessToken, {
+        .cookie("token", token, {
           httpOnly: true,
           sameSite: "None",
           secure: true,
