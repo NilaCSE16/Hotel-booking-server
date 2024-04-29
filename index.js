8515d8ffe97ccf9b3fe667f4fa610faf7efd59aa
NilaCSE16 <112371425+NilaCSE16@users.noreply.github.com>
2024-04-29 20:07:31 +0600

    change vercel file

--- index.js
+++ index.js
@@ -20,34 +20,6 @@ app.use(
 );
 app.use(cookieParser());
 
-// const whitelist = ["*"];
-
-// app.use((req, res, next) => {
-//   const origin = req.get("referer");
-//   const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-//   if (isWhitelisted) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader(
-//       "Access-Control-Allow-Methods",
-//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     );
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "X-Requested-With,Content-Type,Authorization"
-//     );
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//   }
-//   // Pass to next layer of middleware
-//   if (req.method === "OPTIONS") res.sendStatus(200);
-//   else next();
-// });
-
-// const setContext = (req, res, next) => {
-//   if (!req.context) req.context = {};
-//   next();
-// };
-// app.use(setContext);
-
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
